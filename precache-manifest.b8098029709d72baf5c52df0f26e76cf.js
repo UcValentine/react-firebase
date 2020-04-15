a43420262a16af36d754d9d5d19ac10a2ad677b8
Valentine <ucval@yahoo.com>
2020-04-15 19:50:01 +0200

    Updates

---
+++ precache-manifest.b8098029709d72baf5c52df0f26e76cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7980169adc2f9972c877d06c2a1408c9",
+    "url": "/react-firebase/index.html"
+  },
+  {
+    "revision": "a6d07386ad5f612b831b",
+    "url": "/react-firebase/static/css/main.af9f6299.chunk.css"
+  },
+  {
+    "revision": "62cc36430a60774cb8ed",
+    "url": "/react-firebase/static/js/2.cb85c11b.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/react-firebase/static/js/2.cb85c11b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6d07386ad5f612b831b",
+    "url": "/react-firebase/static/js/main.f22d4c30.chunk.js"
+  },
+  {
+    "revision": "08167fc2919696304640",
+    "url": "/react-firebase/static/js/runtime-main.7d5c5837.js"
+  }
+]);
